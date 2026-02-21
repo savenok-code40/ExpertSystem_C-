760131cc856d0f9eb0847089f9a1b03fb27ac1a6
Savenok Mikhail <ozereche2015@gmail.com>
2026-02-21 11:20:37 +0300

    создан класс ForwardChaining.cs

---
+++ InferenceEngine/ForwardChaining.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpertBase
+{
+    public class ForwardChaining
+    {
+        private DataBase db; // поле для хранения базы знаний
+        public ForwardChaining(DataBase dataBase)
+        {
+            this.db = dataBase;
+        }
+
+        // Метода проверки заданной цели в прямом выводе
+        public void Compute(List<Fact> factsInemory, Fact purposeFact, StringBuilder output)
+        {
+
+        }
+
+    }
+}
